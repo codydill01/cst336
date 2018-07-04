73045f46feeea56725aeb9ca428231f4a56c3f3c
codydill01 <codybdill@gmail.com>
2018-07-03 22:07:33 -0700

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ echo "Cody Dill's CST336 Internet Programming";
     <a href = "labs/lab1/"> Lab 1 </a>
     <br>
     <a href = "labs/lab2"/> Lab 2 </a>
+    <br>
+    <a href = "labs/lab3"/> Lab 3 </a>
     <h2> HW</h2>
     <a href = "hw/hw1"> HW 1</a>
     <br>
